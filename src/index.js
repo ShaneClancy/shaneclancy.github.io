414ac42250cdc7cd60cec8f443884b7fb1beb887
Shane Clancy <clancys@oregonstate.edu>
2020-01-10 17:43:33 -0800

    created nav components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class App extends React.Component {
+    render() {
+        return (
+            <Nav/>
+        );
+    }
+}
 
+class Nav extends React.Component {
+    render() {
+        return(
+            <div className="nav">
+                <NavItem text={"Shane Clancy"} link={"/"}/>
+            </div>
+        );
+    }
+}
+
+class NavItem extends React.Component {
+    render() {
+        return <a className="nav-item" href={this.props.link}>{this.props.text}</a> ;
+    }
+}
 
 ReactDOM.render(
     <App />,
