04d46b0a613c5225305f563de8c1800d168905a6
Shane Clancy <clancys@oregonstate.edu>
2020-01-10 18:25:30 -0800

    added transitions for nav-item

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@ import './index.css';
 class App extends React.Component {
     render() {
         return (
+            <Css/>,
             <Nav/>
+        );
+    }
+}
+
+class Css extends React.Component {
+    render() {
+        return (
+            <head>
+                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+            </head>
         );
     }
 }
@@ -15,14 +26,31 @@ class Nav extends React.Component {
         return(
             <div className="nav">
                 <NavItem text={"Shane Clancy"} link={"/"}/>
+                <NavIcon/>
             </div>
         );
     }
 }
 
 class NavItem extends React.Component {
+
     render() {
-        return <a className="nav-item" href={this.props.link}>{this.props.text}</a> ;
+        return (
+            <div className="nav-comp">
+                <a className="nav-item" href={this.props.link}>{this.props.text}</a>
+            </div>
+        );
+    }
+}
+
+class NavIcon extends React.Component {
+
+    render() {
+        return (
+            <div className="nav-comp">
+                <a className="fa fa-github" href="https://github.com/shaneclancy"> </a>
+            </div>
+        );
     }
 }
 
